adf966f3e47e2a40dc5b3538333e72c227a56ea1
andreyferraz <andrey.developer@hotmail.com>
2026-04-04 19:50:10 -0300

    add Usuario entity with attributes and JPA annotations

---
+++ src/main/java/com/mgrecheados/cardapio/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.mgrecheados.cardapio.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String username;
+    private String password;
+    private String role;
+
+}
